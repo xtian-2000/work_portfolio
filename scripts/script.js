cd57cdb1b210b7d2f5174aef8918b69b39e1aa6f
xtian-2000 <christian.gealone1@gmail.com>
2022-07-21 08:16:07 +0800

    - removed the div in div.element in media query

--- scripts/script.js
+++ scripts/script.js
@@ -45,10 +45,22 @@ $(document).ready(function(){
 
     $('#menu-button').click(function(){
         const menu_icon = document.getElementById("menu-icon")
+        const nav_link = document.getElementById("nav-link");
         if (toggleStatus == 0) {
+            // change menu icon to close
             menu_icon.src="https://img.icons8.com/ios/35/000000/delete-sign--v1.png";
-            $('.nav-logo').hide();
+
+            // show nav-link
+            nav_link.style.display = "grid";
+            nav_link.style.gridColumn = "1 / span 2";
+            nav_link.style.gridRow = "2";
+            nav_link.style.padding = "10px";
+            
+            // $("#nav-link").toggleClass("nav-link-menu-on");
+
+            // change toggle status to 1
             toggleStatus = 1;
+            
         } else if (toggleStatus == 1) {
             menu_icon.src="https://img.icons8.com/ios-glyphs/35/000000/menu--v1.png";
             $('.nav-logo').show();
