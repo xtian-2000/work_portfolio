e4291f5be4a59821015553cc3105159b0d98131b
xtian-2000 <christian.gealone1@gmail.com>
2022-07-23 11:29:28 +0800

    - display grid the nav-link when toggleStatus = 1 - used hide method when toggleStatus = 0 in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -48,13 +48,20 @@ $(document).ready(function(){
         // create constants
         const menu_icon = document.getElementById("menu-icon")
         const nav_link = document.getElementById("nav-link");
-        
+
         if (toggleStatus == 0) {
             // change menu icon to close
             menu_icon.src="https://img.icons8.com/ios/35/000000/delete-sign--v1.png";
 
             // use toggle to show/hide the div
-            $("#nav-link").toggle();
+            //$("#nav-link").toggle();
+            // show nav-link
+
+            nav_link.style.display = "grid";
+            nav_link.style.gridColumn = "1 / span 2";
+            nav_link.style.gridRow = "2";
+            nav_link.style.padding = "10px";
+
 
             // change toggle status to 1
             toggleStatus = 1;
@@ -64,7 +71,7 @@ $(document).ready(function(){
             menu_icon.src="https://img.icons8.com/ios-glyphs/35/000000/menu--v1.png";
             
             // use toggle to show/hide the div
-            $("#nav-link").toggle();
+            $("#nav-link").hide();
             
             // change toggle status to 1
             toggleStatus = 0;
