eae9751de897781b68c1e3d7a81f94528e5c4351
xtian-2000 <christian.gealone1@gmail.com>
2022-07-23 11:11:51 +0800

    - created comments in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -44,20 +44,29 @@ $(document).ready(function(){
     var toggleStatus = 0;
 
     $('#menu-button').click(function(){
+
+        // create constants
         const menu_icon = document.getElementById("menu-icon")
         const nav_link = document.getElementById("nav-link");
+        
         if (toggleStatus == 0) {
             // change menu icon to close
             menu_icon.src="https://img.icons8.com/ios/35/000000/delete-sign--v1.png";
 
+            // use toggle to show/hide the div
             $("#nav-link").toggle();
 
             // change toggle status to 1
             toggleStatus = 1;
 
         } else if (toggleStatus == 1) {
+            // change close icon to menu
             menu_icon.src="https://img.icons8.com/ios-glyphs/35/000000/menu--v1.png";
+            
+            // use toggle to show/hide the div
             $("#nav-link").toggle();
+            
+            // change toggle status to 1
             toggleStatus = 0;
         }
       });  
